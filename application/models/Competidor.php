d5078df6c66178f154c4a7ffc7c493eb75dbcbc8
Daniel Pavez <kirito.furukawa@gmail.com>
2017-11-20 04:52:55 -0300

    Competidor completo / Equipo parcial
    CodeIgniter configurado para PostgreSQL.
    Queries de Competidores listas.
    Queríes de Equipo en proceso.

--- application/models/Competidor.php
+++ application/models/Competidor.php
@@ -1,7 +1,63 @@
 <?php
 
 class Competidor extends CI_Model{
-    public function CrearPiloto(){}
-    public function CrearCopiloto(){}
-    public function ModificarCompetidor(){}
+    //Desde su creación se determina su equipo
+    public function CrearPiloto($rut, $nacionalidad, $nombres, $apPaterno, $apMaterno, $nombreEquipo){
+        $data = array(
+            'rut' => $rut,
+            'nacionalidad' => $nacionalidad,
+            'nombres' => $nombres,
+            'apPaterno' => $apPaterno,
+            'apMaterno' => $apMaterno,
+            'nombreEquipo' => $nombreEquipo
+        );
+        $this->db->insert('competidor', $data);
+        $this->db->insert('piloto', array('rutCompetidor' => $rut));
+        //Actualizar cantidad de pilotos del equipo
+
+    }
+    public function CrearCopiloto($rut, $nacionalidad, $nombres, $apPaterno, $apMaterno, $nombreEquipo){
+        $data = array(
+            'rut' => $rut,
+            'nacionalidad' => $nacionalidad,
+            'nombres' => $nombres,
+            'apPaterno' => $apPaterno,
+            'apMaterno' => $apMaterno,
+            'nombreEquipo' => $nombreEquipo
+        );
+        $this->db->insert('competidor', $data);
+        $this->db->insert('copiloto', array('rutCompetidor' => $rut));
+        //Actualizar cantidad de copilotos del equipo
+
+    }
+    public function ModificarCompetidor($rut, $nacionalidad, $nombres, $apPaterno, $apMaterno, $nombreEquipo){
+        $data = array(
+            'nacionalidad' => $nacionalidad,
+            'nombres' => $nombres,
+            'apPaterno' => $apPaterno,
+            'apMaterno' => $apMaterno,
+            'nombreEquipo' => $nombreEquipo
+        );
+        $this->db->where('rut', $rut);
+        $this->db->update('competidor', $data);
+    }
+
+    public function ObtenerCompetidores(){
+        $query = $this->db->get('competidor');
+        return $query->result_array();
+    }
+    public function ObtenerPilotos(){
+        $this->db->select('rut', 'nacionalidad', 'nombres', 'apPaterno', 'apMaterno', 'nombreEquipo');
+        $this->db->from('competidor');
+        $this->db->join('piloto', 'piloto.rutCompetidor = competidor.rut', 'inner');
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+    public function ObtenerCopilotos(){
+        $this->db->select('rut', 'nacionalidad', 'nombres', 'apPaterno', 'apMaterno', 'nombreEquipo');
+        $this->db->from('competidor');
+        $this->db->join('copiloto', 'copiloto.rutCompetidor = competidor.rut', 'inner');
+        $query = $this->db->get();
+        return $query->result_array();
+    }
 }
--- application/models/Equipo.php
+++ application/models/Equipo.php
@@ -1,15 +1,29 @@
 <?php
 
 class Equipo extends CI_Model{
-    public function CrearEquipo(){}
-    public function ModificarEquipo(){}
-    public function AgregarCompetidor(){}
-    public function AgregarAutomovil(){}
-    public function EliminarCompetidor(){}
-    public function EliminarAutomovil(){}
+    public function CrearEquipo($nombre){
+        $data = array(
+            'nombre' => $nombre,
+            'numPilotos' => 0,
+            'numCopilotos' => 0
+        );
+        $this->db->insert('equipo', $data);
+    }
+    public function ModificarEquipo($nombre){
+        $this->db->set('nombre',$nombre);
+        $this->db->update('equipo');
+    }
+    public function AgregarAutomovil($nombre, $matricula){
 
-    public function ObtenerCompetidores(){}
-    public function ObtenerPilotos(){}
-    public function ObtenerCopilotos(){}
+    }
+    public function EliminarAutomovil($nombre, $matricula){ //¿Se puede?
+
+    }
+
+    //¿Modificar/Eliminar competidor de un equipo/ cambiarlo de equipo?
+
+    public function ObtenerCompetidoresEquipo(){} //3 join creo :D
+    public function ObtenerPilotosEquipo(){}
+    public function ObtenerCopilotosEquipo(){}
     public function ObtenerAutomoviles(){}
 }
